49d35023924e50a31395a57a8e61ba4409b2a871
9965602493 <48362358+9965602493@users.noreply.github.com>
2019-03-09 15:11:00 +0530

    Add files via upload

--- java/com/example/codiecon/MainActivity.java
+++ java/com/example/codiecon/MainActivity.java
@@ -5,23 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView but,but1,but2;
+    EditText com_reg_name,com_reg_pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final EditText com_reg_name = (EditText) findViewById(R.id.user_id);
+        final EditText com_reg_pass = (EditText) findViewById(R.id.password);
         but2 = findViewById(R.id.button);
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,market_login.class);
-                startActivity(intent);
-            }
+
+                String content = com_reg_name.getText().toString();
+                String content1 = com_reg_pass.getText().toString();
+
+                if(content.equals("employee")) {
+
+                    Intent intent = new Intent(MainActivity.this,company_login.class);
+                    startActivity(intent);
+
+                }
+                else
+                {
+                    Intent intent = new Intent(MainActivity.this,market_login.class);
+                    startActivity(intent);
+
+                }
+
+
+
+
+                }
         });
+
+
 
         but = findViewById(R.id.reg_link);
         but.setOnClickListener(new View.OnClickListener() {
--- java/com/example/codiecon/forget_pass.java
+++ java/com/example/codiecon/forget_pass.java
@@ -1,13 +1,43 @@
 package com.example.codiecon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class forget_pass extends AppCompatActivity {
-
+    Button but2;
+    TextView textView1;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_pass);
+
+        but2 = findViewById(R.id.dback);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                textView1= findViewById(R.id.update);
+                textView1.setText("Password updated !!! ");
+
+                timer =new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(forget_pass.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+            }
+        });
+
     }
 }
